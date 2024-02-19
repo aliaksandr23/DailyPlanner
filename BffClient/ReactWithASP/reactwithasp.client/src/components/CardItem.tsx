e97ed1073d93b09f7291fe6c8fcb687f16b62d69
Alexander Zayats <A.Zayats98@gmail.com>
2024-02-19 13:42:43 +0300

    Add modal to view card details.

--- BffClient/ReactWithASP/reactwithasp.client/src/components/CardItem.tsx
+++ BffClient/ReactWithASP/reactwithasp.client/src/components/CardItem.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Card } from "../types/types";
+import { Modal } from "./UI/Modal/Modal";
 
 interface CardItemProps {
     card: Card
@@ -6,12 +8,23 @@ interface CardItemProps {
 
 const CardItem: React.FC<CardItemProps> = ({ card }) => {
     const { title } = card;
+    const [isCardDataModalOpen, setCardDataModalOpen] = useState<boolean>(false);
+    const handleOpenModal = () => {
+        setCardDataModalOpen(true);
+    }
+    const handleCloseModal = () => {
+        setCardDataModalOpen(false);
+    }
 
     return (
-        <li className="card-item">
-            <h4 className="card-title">{title}</h4>
-            <h4 className="card-title">{card.id}</h4>
-        </li>
+        <>
+            <li className="card-item" onClick={handleOpenModal}>
+                <h4 className="card-title">{title}</h4>
+                <h4 className="card-title">{card.id}</h4>
+            </li>
+            <Modal title={title} onClose={handleCloseModal} visible={isCardDataModalOpen}>
+            </Modal>
+        </>
     );
 }
 
