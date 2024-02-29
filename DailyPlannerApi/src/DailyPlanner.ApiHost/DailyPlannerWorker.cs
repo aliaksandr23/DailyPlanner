52227f92d92c3b1895aac62b25a8dd58eac6d0eb
Alexander Zayats <A.Zayats98@gmail.com>
2024-02-29 14:42:47 +0300

    Remove default data-seeds.

--- DailyPlannerApi/src/DailyPlanner.ApiHost/DailyPlannerWorker.cs
+++ DailyPlannerApi/src/DailyPlanner.ApiHost/DailyPlannerWorker.cs
@@ -12,11 +12,8 @@ namespace DailyPlanner.ApiHost
         public async Task StartAsync(CancellationToken cancellationToken = default)
         {
             await using var scope = _serviceProvider.CreateAsyncScope();
-
             var _initializer = scope.ServiceProvider.GetRequiredService<DailyPlannerDbInitializer>();
-
             await _initializer.InitializeAsync(cancellationToken);
-            await _initializer.SeedAsync(cancellationToken);
         }
 
         public Task StopAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
--- DailyPlannerApi/src/DailyPlanner.Infrastructure/Data/DailyPlannerDbInitializer.cs
+++ DailyPlannerApi/src/DailyPlanner.Infrastructure/Data/DailyPlannerDbInitializer.cs
@@ -9,7 +9,6 @@ namespace DailyPlanner.Infrastructure.Data
     public class DailyPlannerDbInitializer
     {
         private readonly DailyPlannerDbContext _context;
-        private readonly IDateTimeService _dateTimeService;
         private readonly ILogger<DailyPlannerDbInitializer> _logger;
 
         public DailyPlannerDbInitializer(DailyPlannerDbContext context,
@@ -17,7 +16,6 @@ namespace DailyPlanner.Infrastructure.Data
         {
             _logger = logger;
             _context = context;
-            _dateTimeService = dateTimeService;
         }
 
         public async Task InitializeAsync(CancellationToken cancellationToken = default)
@@ -30,70 +28,6 @@ namespace DailyPlanner.Infrastructure.Data
             {
                 _logger.LogError(ex, "An error occurred while initializing the database");
                 throw;
-            }
-        }
-
-        public async Task SeedAsync(CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                await TrySeedAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "An error occurred while seeding the database");
-                throw;
-            }
-        }
-
-        private async Task TrySeedAsync(CancellationToken cancellationToken = default)
-        {
-            if (!_context.Boards.Any())
-            {
-                await _context.Boards.AddAsync(new Board
-                {
-                    Title = "Daily Planner",
-                    IsPrivate = true,
-                    IsFavorite = true,
-                    Columns = new List<Column>
-                    {
-                        new() { Title = "Is done" },
-                        new() {
-                            Title = "Needs to be done",
-                            Cards = new List<Card>
-                            {
-                                new() {
-                                    Title = "Create Identity project",
-                                    Description = "Сreate an API that will authenticate and authorize users and store their personal data",
-                                    Priority = CardPriority.Medium,
-                                    StartDate = _dateTimeService.Now.AddDays(5),
-                                    EndDate = _dateTimeService.Now.AddDays(14),
-                                },
-                                new() {
-                                    Title = "Create Client",
-                                    Description = "Сreate a React project through which users will interact with the application",
-                                    Priority = CardPriority.Medium,
-                                    StartDate = _dateTimeService.Now.AddDays(14),
-                                }
-                            }
-                        },
-                        new() {
-                            Title = "In progress",
-                            Cards = new List<Card>
-                            {
-                                new() {
-                                    Title = "Сreate tests",
-                                    Description = "Сreate unit and integration tests for each required level of the application",
-                                    Priority = CardPriority.High,
-                                    StartDate = _dateTimeService.Now,
-                                    EndDate = _dateTimeService.Now.AddDays(7),
-                                }
-                            }
-                        },
-                    }
-                }, cancellationToken);
-
-                await _context.SaveChangesAsync(cancellationToken);
             }
         }
     }
